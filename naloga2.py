241ffedc2744bc4a956a268fc315e8453ceb238a
zanlah <zan.lah1@student.um.si>
2024-03-30 12:45:34 +0100

    Initial commit

---
+++ naloga2.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+
+def konvolucija(slika, jedro):
+    '''Izvede konvolucijo nad sliko. Brez uporabe funkcije cv.filter2D, ali katerekoli druge funkcije, ki izvaja konvolucijo.
+    Funkcijo implementirajte sami z uporabo zank oz. vektorskega računanja.'''
+    pass
+
+def filtriraj_z_gaussovim_jedrom(slika,sigma):
+    '''Filtrira sliko z Gaussovim jedrom..'''
+    pass
+
+def filtriraj_sobel_smer(slika):
+    '''Filtrira sliko z Sobelovim jedrom in označi gradiente v orignalni sliki glede na ustrezen pogoj.'''
+    pass
+
+if __name__ == '__main__':    
+    pass
